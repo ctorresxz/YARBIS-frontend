3d4306196a85a5d6745903407d8471b61778cde5
Carlos Torres <torresxz88@gmail.com>
2025-10-08 08:19:52 -0500

    add:tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: { themes: ["corporate", "light", "dark"] },
+};
+
+export default config;
